3acdcd1445e197894932dac653062df9aa5fc2d9
Odia Adodo <subzbelow@gmail.com>
2023-03-14 16:30:03 +0100

    Update

--- Activity.js
+++ Activity.js
@@ -1,84 +1,131 @@
 function Activity(amount = 0) {
-    this.amount = amount;
+  this.amount = amount;
 }
 Activity.prototype.setAmount = function (value = "") {
-  if(value<=0)return false;
-  else{this.amount=value; return true}
+  if (value <= 0) return false;
+  else {
+    this.amount = value;
+    return true;
+  }
 };
 Activity.prototype.getAmount = function () {
   return this.amount;
 };
 
-
 function Payment(amount = 0, receiver = "") {
-    Activity.call(this);
-    this.amount = amount;
-    this.receiver = receiver;
+  Activity.call(this);
+  this.amount = amount;
+  this.receiver = receiver;
 }
 Payment.prototype = Object.create(Activity.prototype);
 Payment.prototype.setReceiver = function (value = "") {
-  this.receiver =value;
+  this.receiver = value;
 };
 Payment.prototype.getReceiver = function () {
   return this.receiver;
 };
 
-function Refund(amount = 0, sender= "") {
-     Activity.call(this);
-     this.amount=amount;
-     this.sender=sender;
-     
+function Refund(amount = 0, sender = "") {
+  Activity.call(this);
+  this.amount = amount;
+  this.sender = sender;
 }
 Refund.prototype = Object.create(Activity.prototype);
 Refund.prototype.setSender = function (value = "") {
-  this.sender =value;
+  this.sender = value;
 };
 Refund.prototype.getSender = function () {
   return this.sender;
 };
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-    
-    const objectType = readLine().trim();
-    
-    const inputsForObjectCreation = readLine().trim().split(' ');
-    const updatedAmount = parseInt(readLine().trim());
-    const updatedSenderReceiver = readLine().trim();
-    switch(objectType) {
-        case 'Payment':
-            const paymentObj = new Payment(parseInt(inputsForObjectCreation[0]), inputsForObjectCreation[1]);
-            ws.write(`Payment object created with amount ${paymentObj.getAmount()} and receiver ${paymentObj.getReceiver()}\n`);
-            if(paymentObj.setAmount(updatedAmount)) {
-                ws.write(`Amount updated to ${updatedAmount}\n`);
-            } else {
-                ws.write(`Amount not updated\n`);
-            }
-            paymentObj.setReceiver(updatedSenderReceiver);
-            ws.write(`Receiver updated to ${updatedSenderReceiver}\n`);
-            ws.write(`Payment object details - amount is ${paymentObj.getAmount()} and receiver is ${paymentObj.getReceiver()}\n`);
-            ws.write(`Payment.prototype has property setAmount: ${Payment.prototype.hasOwnProperty('setAmount')}\n`);
-            ws.write(`Payment.prototype has property getAmount: ${Payment.prototype.hasOwnProperty('getAmount')}\n`);
-            ws.write(`Payment.prototype has property setReceiver: ${Payment.prototype.hasOwnProperty('setReceiver')}\n`);
-            ws.write(`Payment.prototype has property getReceiver: ${Payment.prototype.hasOwnProperty('getReceiver')}\n`);
-            break;
-        case 'Refund':
-            const refundObj = new Refund(parseInt(inputsForObjectCreation[0]), inputsForObjectCreation[1]);
-            ws.write(`Refund object created with amount ${refundObj.getAmount()} and sender ${refundObj.getSender()}\n`);
-            if(refundObj.setAmount(updatedAmount)) {
-                ws.write(`Amount updated to ${updatedAmount}\n`);
-            } else {
-                ws.write(`Amount not updated\n`);
-            }
-            refundObj.setSender(updatedSenderReceiver);
-            ws.write(`Sender updated to ${updatedSenderReceiver}\n`);
-            ws.write(`Refund object details - amount is ${refundObj.getAmount()} and sender is ${refundObj.getSender()}\n`);
-            ws.write(`Refund.prototype has property setAmount: ${Refund.prototype.hasOwnProperty('setAmount')}\n`);
-            ws.write(`Refund.prototype has property getAmount: ${Refund.prototype.hasOwnProperty('getAmount')}\n`);
-            ws.write(`Refund.prototype has property setSender: ${Refund.prototype.hasOwnProperty('setSender')}\n`);
-            ws.write(`Refund.prototype has property getSender: ${Refund.prototype.hasOwnProperty('getSender')}\n`);
-            break;
-        default:
-            break;
-    }
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const objectType = readLine().trim();
+
+  const inputsForObjectCreation = readLine().trim().split(" ");
+  const updatedAmount = parseInt(readLine().trim());
+  const updatedSenderReceiver = readLine().trim();
+  switch (objectType) {
+    case "Payment":
+      const paymentObj = new Payment(
+        parseInt(inputsForObjectCreation[0]),
+        inputsForObjectCreation[1]
+      );
+      ws.write(
+        `Payment object created with amount ${paymentObj.getAmount()} and receiver ${paymentObj.getReceiver()}\n`
+      );
+      if (paymentObj.setAmount(updatedAmount)) {
+        ws.write(`Amount updated to ${updatedAmount}\n`);
+      } else {
+        ws.write(`Amount not updated\n`);
+      }
+      paymentObj.setReceiver(updatedSenderReceiver);
+      ws.write(`Receiver updated to ${updatedSenderReceiver}\n`);
+      ws.write(
+        `Payment object details - amount is ${paymentObj.getAmount()} and receiver is ${paymentObj.getReceiver()}\n`
+      );
+      ws.write(
+        `Payment.prototype has property setAmount: ${Payment.prototype.hasOwnProperty(
+          "setAmount"
+        )}\n`
+      );
+      ws.write(
+        `Payment.prototype has property getAmount: ${Payment.prototype.hasOwnProperty(
+          "getAmount"
+        )}\n`
+      );
+      ws.write(
+        `Payment.prototype has property setReceiver: ${Payment.prototype.hasOwnProperty(
+          "setReceiver"
+        )}\n`
+      );
+      ws.write(
+        `Payment.prototype has property getReceiver: ${Payment.prototype.hasOwnProperty(
+          "getReceiver"
+        )}\n`
+      );
+      break;
+    case "Refund":
+      const refundObj = new Refund(
+        parseInt(inputsForObjectCreation[0]),
+        inputsForObjectCreation[1]
+      );
+      ws.write(
+        `Refund object created with amount ${refundObj.getAmount()} and sender ${refundObj.getSender()}\n`
+      );
+      if (refundObj.setAmount(updatedAmount)) {
+        ws.write(`Amount updated to ${updatedAmount}\n`);
+      } else {
+        ws.write(`Amount not updated\n`);
+      }
+      refundObj.setSender(updatedSenderReceiver);
+      ws.write(`Sender updated to ${updatedSenderReceiver}\n`);
+      ws.write(
+        `Refund object details - amount is ${refundObj.getAmount()} and sender is ${refundObj.getSender()}\n`
+      );
+      ws.write(
+        `Refund.prototype has property setAmount: ${Refund.prototype.hasOwnProperty(
+          "setAmount"
+        )}\n`
+      );
+      ws.write(
+        `Refund.prototype has property getAmount: ${Refund.prototype.hasOwnProperty(
+          "getAmount"
+        )}\n`
+      );
+      ws.write(
+        `Refund.prototype has property setSender: ${Refund.prototype.hasOwnProperty(
+          "setSender"
+        )}\n`
+      );
+      ws.write(
+        `Refund.prototype has property getSender: ${Refund.prototype.hasOwnProperty(
+          "getSender"
+        )}\n`
+      );
+      break;
+    default:
+      break;
+  }
 }
