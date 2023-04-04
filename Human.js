d02ff1bf7b0703d4e1e3e392d5893d15548f18c8
Odia Adodo <subzbelow@gmail.com>
2023-04-04 20:40:54 +0100

    Introduced Unit tests with Chai

--- Human.js
+++ Human.js
@@ -1,212 +1,206 @@
-//is_a cannot be instantiated.
-let is_a = function () {
-  this.man = true;
-  this.woman = true;
-  //throw invalid operation exception.
-};
-
-// is_not_a cannot be instantiated.
-let is_not_a = function () {
-  this.man = false;
-  this.woman = false;
-  //throw invalid operation exception.
-};
-
-// base obj cannot be instantiated.
-let baseObj = function () {
-  this.name;
-  this.is_a = is_a;
-  this.is_not_a = is_not_a;
- //throw invalid operation exception.
-};
-
 //Thing
-let Thing = function (name = "") {
-  baseObj.call(this);
-  this.name = name;
-  this.is_a_woman = this.is_a.woman;
-  this.is_a_man = !this.is_a_woman;
-  this.is_not_a.man = this.is_a_woman;
-  this.is_not_a.woman = this.is_a_man;
-  this.parent_of = "";
-  this.speech = [""];
-  this.arms = [arm];
-  this.hands = [hand];
-  this.head = head;
-};
-Thing.prototype = Object.create(baseObj.prototype);
-
-Thing.prototype.speak = function (phrase = "") {
-  let result = `${this.name} says: ${phrase}!`;
-  this.speech.push(result);
-  return result;
-};
-
-Thing.prototype.is_the = function () {
-  let ans = { parent_of: { joe: "joe", kiwi: "kiwi" } };
-  return ans;
-};
-
-Thing.prototype.spoke = function () {
-  return this.speech;
-};
-
-Thing.prototype.can = function () {
-  let result = {
-    speak:
-      () =>
-      (phrase = "") =>
-        `${this.name} says: ${phrase}!`,
-    speak: () => ("spoke", (phrase = "") => `${this.name} says: ${phrase}!`),
-  };
-  return result;
-};
-
-Thing.prototype.has = function (amount = 0) {
-  let arms = (amount) => {
-    let element = [arm];
-    for (let i = 0; i < amount; i++) {
-      element.push(new arm());
+class Thing {
+    constructor(name = "") {
+      this.name = name;
+      this.is_not_a = { man: false, woman: false };
+      this.is_a = { man: !this.is_not_a.woman, woman: !this.is_not_a.man };
+      this.parent_of = "";
+      this.speech = [""];
+      this.arms = [arm];
+      this.hands = [hand];
+      this.head = head;
     }
-    if (element.length > 1) return element;
-    return new arm();
-  };
-
-  let fingers = (amount) => {
-    let element = [finger];
-    for (let i = 0; i < amount; i++) {
-      element.push(new finger());
+    speak(phrase = "") {
+      let result = `${this.name} says: ${phrase}!`;
+      this.speech.push(result);
+      return result;
     }
-    if (element.length > 1) return element;
-    return new finger();
-  };
-
-  let hands = (amount) => {
-    let element = [hand];
-    for (let i = 0; i < amount; i++) {
-      element.push(new hand());
+    is_the() {
+      let ans = { parent_of: { joe: "joe", kiwi: "kiwi" } };
+      return ans;
     }
-    if (element.length > 1) return element;
-    return new hand();
-  };
-
-  let he = (amount) => {
-    let element = [head];
-    for (let i = 0; i < amount; i++) {
-      element.push(new head());
+    spoke() {
+      return this.speech;
     }
-    if (element.length > 1) return element;
-    return new head();
-  };
-  let result = {
-    arms: (this.arms = arms()),
-    head: (this.head = he()),
-    fingers: fingers(),
-    hands: (this.hands = hands()),
-  };
-  return result;
-};
-
-//arm
-let arm = function () {
-  Thing.call(this);
-  this.name = "arm";
-};
-arm.prototype = Object.create(Thing.prototype);
-arm.prototype.move = function (param = true) {
-  this.isManager = param;
-};
-
-//hands
-let hand = function () {
-  Thing.call(this);
-  this.name = "hand";
-  this.fingers = [finger];
-  this.arm = arm;
-  this.fingers.length = this.having().fingers;
-};
-hand.prototype = Object.create(Thing.prototype);
-hand.prototype.having = function (amount = 0) {
-  let fingers = (amount) => {
-    let element = [eye];
-    for (let i = 0; i < amount; i++) {
-      element.push(new eye());
+    can() {
+      let result = {
+        speak:
+          () =>
+          (phrase = "") =>
+            `${this.name} says: ${phrase}!`,
+        speak: () => ("spoke", (phrase = "") => `${this.name} says: ${phrase}!`),
+      };
+      return result;
     }
-    if (element.length > 1) return element;
-    return new eye();
-  };
-  let result = {
-    eyes: (this.fingers = fingers()),
-    nose: amount,
-    mouth: amount,
-  };
-  return result;
-};
-
-//Fingers
-let finger = function () {
-  baseObj.call(this);
-  this.name = "finger";
-};
-finger.prototype = Object.create(Thing.prototype);
-
-//head
-let head = function () {
-  Thing.call(this);
-  this.name = "head";
-  this.eyes = [eye];
-};
-head.prototype = Object.create(Thing.prototype);
-head.prototype.having = function (amount = 0) {
-  let eyes = (amount) => {
-    let element = [eye];
-    for (let i = 0; i < amount; i++) {
-      element.push(new eye());
+    has(amount = 0) {
+      let arms = (amount) => {
+        let element = [arm];
+        for (let i = 0; i < amount; i++) {
+          element.push(new arm());
+        }
+        if (element.length > 1) return element;
+        return new arm();
+      };
+  
+      let fingers = (amount) => {
+        let element = [finger];
+        for (let i = 0; i < amount; i++) {
+          element.push(new finger());
+        }
+        if (element.length > 1) return element;
+        return new finger();
+      };
+  
+      let hands = (amount) => {
+        let element = [hand];
+        for (let i = 0; i < amount; i++) {
+          element.push(new hand("hand"));
+        }
+        if (element.length > 1) return element;
+        return new hand("hand");
+      };
+  
+      let he = (amount) => {
+        let element = [head];
+        for (let i = 0; i < amount; i++) {
+          element.push(new head("hand"));
+        }
+        if (element.length > 1) return element;
+        return new head();
+      };
+      let result = {
+        arms: (this.arms = arms()),
+        head: (this.head = he()),
+        fingers: fingers(),
+        hands: () => this.hands = hands(),
+      };
+      return result;
     }
-    if (element.length > 1) return element;
-    return new eye();
-  };
-  let result = { eyes: (this.eyes = eyes()), nose: amount, mouth: amount };
-  return result;
-};
-
-//eye
-let eye = function () {
-  Thing.call(this);
-  this.name = "eye";
-  this.color = "";
-  this.shape = "";
-};
-eye.prototype = Object.create(Thing.prototype);
-eye.prototype.being_the = function () {
-  let result = {
-    color: {
-      red: () => (this.color = "red"),
-      blue: () => (this.color = "blue"),
-      green: () => (this.color = "green"),
-      brown: () => (this.color = "brown"),
-      red: {
-        and_the: {
-          shape: {
-            round: () => ((this.shape = "round"), (this.color = "red")),
+  }
+  //Thing.prototype = Object.create(baseObj.prototype);
+  
+  //arm
+  class arm extends Thing {
+    constructor(name = "arm") {
+      //Thing.call(this);
+      super(name);;
+    }
+  }
+  //arm.prototype = Object.create(Thing.prototype);
+  
+  //hands
+  class hand extends Thing {
+    constructor(name = "hand") {;
+      super(name);
+      this.fingers = [finger];
+      this.arm = arm;
+    }
+    having(amount = 0) {
+      let fingers = (amount) => {
+        let element = [finger];
+        for (let i = 0; i < amount; i++) {
+          element.push(new finger());
+        }
+        if (element.length > 1) return element;
+        return new finger("finger");
+      };
+      let result = {
+        fingers: () => {this.fingers = fingers()},
+        palm: amount
+      };
+      return result;
+    }
+  }
+  //hand.prototype = Object.create(Thing.prototype);
+  
+  //Fingers
+  class finger extends Thing {
+    constructor(name = "finger") {
+      //Thing.call(this);
+      super(name);
+    }
+  }
+  
+  //head
+  class head extends Thing {
+    constructor(name = "head") {
+      //Thing.call(this);
+      super(name);
+      this.eyes = [eye];
+    }
+    having(amount = 0) {
+      let eyes = (amount) => {
+        let element = [eye];
+        for (let i = 0; i < amount; i++) {
+          element.push(new eye());
+        }
+        if (element.length > 1) return element;
+        return new eye("eye");
+      };
+      let result = { eyes: (this.eyes = eyes()), nose: amount, mouth: amount };
+      return result;
+    }
+  }
+  //head.prototype = Object.create(Thing.prototype);
+  
+  //eye
+  class eye extends Thing {
+    constructor(name = "eye") {
+      super(name);
+      this.color = "";
+      this.shape = "";
+    }
+  
+  }
+  
+  function being_the() {
+      let result = {
+        color: {
+          red: () => (this.color = "red"),
+          blue: () => (this.color = "blue"),
+          green: () => (this.color = "green"),
+          brown: () => (this.color = "brown"),
+          red: {
+            and_the: {
+              shape: {
+                round: () => ((this.shape = "round"), (this.color = "red")),
+              },
+            },
+          },
+          blue: {
+            and_the: {
+              shape: {
+                round: () => ((this.shape = "round"), (this.color = "blue")),
+              },
+            },
+          },
+          red: {
+            and_the: {
+              shape: {
+                round: () => ((this.shape = "round"), (this.color = "red")),
+              },
+            },
           },
         },
-      },
-      blue: {
-        and_the: {
-          shape: {
-            round: () => ((this.shape = "round"), (this.color = "blue")),
-          },
-        },
-      },
-      red: {
-        and_the: {
-          shape: {
-            round: () => ((this.shape = "round"), (this.color = "red")),
-          },
-        },
-      },
-    },
-  };
-  return result;
-};
+      };
+      return result;
+    }
+
+//having
+function having(amount = 0) {
+    let fingers = (amount) => {
+      let element = [finger];
+      for (let i = 0; i < amount; i++) {
+        element.push(new finger());
+      }
+      if (element.length > 1) return element;
+      return new finger("finger");
+    };
+    let result = {
+      fingers: () => {this.fingers = fingers()},
+      palm: amount
+    };
+    return result;
+  }
+
+  
